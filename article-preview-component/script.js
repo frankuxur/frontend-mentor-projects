10f2c680b535e759a7ebde6bde52a7a6a110bd2f
frankuxur <69151130+frankuxur@users.noreply.github.com>
2023-05-09 02:29:05 +0530

    initial commit

---
+++ article-preview-component/script.js
@@ -0,0 +1,9 @@
+const share = document.querySelector('.share')
+const shareBtn = document.querySelector('.share__btn')
+const socials = document.querySelector('.socials')
+
+shareBtn.addEventListener('click', () => {
+    share.classList.toggle('active')
+})
+
+// frankuxur

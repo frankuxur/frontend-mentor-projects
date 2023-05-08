c6fff233eca39bd30159ad05922b5a8ee5974721
frankuxur <69151130+frankuxur@users.noreply.github.com>
2023-05-09 02:24:52 +0530

    Delete article-preview-component directory

--- article-preview-component/script.js
+++
@@ -1,9 +0,0 @@
-const share = document.querySelector('.share')
-const shareBtn = document.querySelector('.share__btn')
-const socials = document.querySelector('.socials')
-
-shareBtn.addEventListener('click', () => {
-    share.classList.toggle('active')
-})
-
-// frankuxur
